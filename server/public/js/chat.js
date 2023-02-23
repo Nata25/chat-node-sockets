abac20801f132cde78f29c91124c1750d510927c
Natalya Ivanova <natalyas.ivanova@gmail.com>
2023-02-23 13:02:48 +0200

    Disable buttons while fetching data or waiting for a message to arrive

--- server/public/js/chat.js
+++ server/public/js/chat.js
@@ -6,11 +6,22 @@ socket.on('message', message => {
   console.log(message);
 });
 
-document.querySelector('#chatForm').addEventListener('submit', function(event) {
+const chatForm = document.querySelector('#chatForm');
+const submitMessageBtn = document.querySelector('#submitMessageBtn');
+const message = document.querySelector('#message');
+const locationBtn = document.querySelector('#locationShare');
+
+chatForm.addEventListener('submit', function(event) {
   event.preventDefault();
+  submitMessageBtn.setAttribute('disabled', 'disabled');
+
   const { value } = event.target.elements.message;
   if (value) {
     socket.emit('sendMessage', value, error => {
+      submitMessageBtn.removeAttribute('disabled');
+      message.value = '';
+      message.focus();
+
       if (error) {
         console.warn(error);
         return;
@@ -22,6 +33,8 @@ document.querySelector('#chatForm').addEventListener('submit', function(event) {
 });
 
 document.querySelector('#locationShare').addEventListener('click', function() {
+  locationBtn.setAttribute('disabled', 'disabled');
+  console.log('Fetching location...');
   geolocationPromisified()
     .then(position => {
       const { coords } = position;
@@ -29,7 +42,8 @@ document.querySelector('#locationShare').addEventListener('click', function() {
         lat: coords.latitude,
         lng: coords.longitude,
       }, () => {
-        console.log('Location was successfully shared.')
+        console.log('Location was successfully shared.');
+        locationBtn.removeAttribute('disabled');
       });
     })
     .catch(e => {
